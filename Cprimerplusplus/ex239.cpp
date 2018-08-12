4a90713b6345ee2b1eb0800f8591aa45fe09080c
junwu <junwuli@foxmail.com>
2018-08-12 13:52:21 +0800

    ex239

---
+++ Cprimerplusplus/ex239.cpp
@@ -0,0 +1,20 @@
+//
+//  ex239.cpp
+//  Cprimerplusplus
+//
+//  Created by junwuli on 2018/8/12.
+//  Copyright © 2018 GDUT. All rights reserved.
+//
+
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+struct Foo{}
+
+int main()
+{
+    
+    return 0;
+}
