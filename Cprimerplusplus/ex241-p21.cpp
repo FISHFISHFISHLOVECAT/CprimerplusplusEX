8fe3150860a3176ed9ed6316760c1c8b0366d390
junwu <junwuli@foxmail.com>
2018-08-12 14:41:31 +0800

    增加了my_Sales_data类的三个成员函数成员

--- Cprimerplusplus/ex241-p21.cpp
+++ Cprimerplusplus/ex241-p21.cpp
@@ -11,11 +11,31 @@
 using namespace std;
 
 struct my_Sales_data{
-    string country;
-    string bookNo;
+    
+    std::string isbn() const{return bookNo;};//这个函数的定义直接在大括号中
+    my_Sales_data & combine(const my_Sales_data &rhs);
+    double avg_price() const;
+    
+    std::string country;
+    std::string bookNo;
     unsigned units_sold=0;
     double revenue=0.0;
 };
+
+double my_Sales_data::avg_price()const{
+    if(units_sold)
+        return revenue/units_sold;//总收入除以卖出总数
+    else
+        return 0;
+}
+
+my_Sales_data& my_Sales_data::combine(const my_Sales_data &rhs)
+{
+    units_sold+=rhs.units_sold;
+    revenue+=rhs.revenue;
+    
+    return *this;
+}
 
 int main()
 {
