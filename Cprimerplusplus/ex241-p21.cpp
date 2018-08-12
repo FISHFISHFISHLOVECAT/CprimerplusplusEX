1a9e5b26b71583f320e6dc57c892a121403c8748
junwu <junwuli@foxmail.com>
2018-08-12 17:28:47 +0800

    写了一个读入写入成员函数，并将其应用。

--- Cprimerplusplus/ex241-p21.cpp
+++ Cprimerplusplus/ex241-p21.cpp
@@ -10,6 +10,8 @@
 #include <iostream>
 using namespace std;
 
+
+
 struct my_Sales_data{
     
     std::string isbn() const{return bookNo;};//这个函数的定义直接在大括号中
@@ -29,21 +31,42 @@ double my_Sales_data::avg_price()const{
         return 0;
 }
 
+istream &read(istream &is,my_Sales_data &item)
+{
+    double price=0;
+    is>>item.country>>item.bookNo>>item.units_sold>>item.revenue;
+    item.revenue=price*item.units_sold;
+    return is;
+}
+
+ostream &print(ostream &os,const my_Sales_data &item)
+{
+    os<<item.country<<" "<<item.isbn()<<" "<<item.units_sold<< " "
+    <<item.revenue<<" "<<item.avg_price();
+    return os;
+}
+
+my_Sales_data add(const my_Sales_data &lhs,const my_Sales_data &rhs)
+{
+    my_Sales_data sum=lhs;
+    sum.combine(rhs);
+    return sum;
+}
+
 my_Sales_data& my_Sales_data::combine(const my_Sales_data &rhs)
 {
     units_sold+=rhs.units_sold;
     revenue+=rhs.revenue;
-    
     return *this;
 }
 
 int main()
 {
     my_Sales_data total;
-    if (cin >> total.bookNo >> total.units_sold >> total.revenue)
+    if (read(cin,total))
     {
         my_Sales_data trans;
-        while (cin >> trans.bookNo >> trans.units_sold >> trans.revenue)
+        while (read(cin,trans))
         {
             if (total.isbn() == trans.isbn())
             {
@@ -51,11 +74,13 @@ int main()
             }
             else
             {
-                cout << total.bookNo << " " << total.units_sold << " " << total.revenue << endl;
+                print(cout,total);
+                cout<<endl;
                 total = trans;
             }
         }
-        cout << total.bookNo << " " << total.units_sold << " " << total.revenue << endl;
+        print(cout,total);
+        cout<<endl;
     }
     else
     {
