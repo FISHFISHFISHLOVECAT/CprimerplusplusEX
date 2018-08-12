d3aaae8fee1b6ba614eaeb326088ccde866474cb
junwu <junwuli@foxmail.com>
2018-08-12 14:54:40 +0800

    创建了一个只有数据成员的类Person

---
+++ Cprimerplusplus/person_info.cpp
@@ -0,0 +1,15 @@
+//
+//  person_info.cpp
+//  Cprimerplusplus
+//
+//  Created by junwuli on 2018/8/12.
+//  Copyright © 2018 GDUT. All rights reserved.
+//
+
+#include <stdio.h>
+#include <iostream>
+
+struct Person{
+    std::string name;//姓名
+    std::string addr;//地址
+}
