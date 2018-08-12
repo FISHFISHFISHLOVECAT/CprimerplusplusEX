623ee34307e0f83b879a4d46eeb5c5f2e1a60ce0
junwu <junwuli@foxmail.com>
2018-08-12 13:58:23 +0800

    ex240

---
+++ Cprimerplusplus/ex240.h
@@ -0,0 +1,19 @@
+//
+//  ex240.h
+//  Cprimerplusplus
+//
+//  Created by junwuli on 2018/8/12.
+//  Copyright © 2018 GDUT. All rights reserved.
+//
+
+#ifndef ex240_h
+#define ex240_h
+
+struct my_Sales_data{
+    std::string country;
+    std::string bookNo；
+    unsigned units_sold=0;
+    double revenue=0.0;
+};
+
+#endif /* ex240_h */
