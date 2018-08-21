dfd2cdc4e39b79b6fd6c1fb877728da3cbffe2a1
junwu <junwuli@foxmail.com>
2018-08-21 09:25:22 +0800

    ex915

---
+++ Cprimerplusplus/test.cpp
@@ -0,0 +1,28 @@
+//
+//  test.cpp
+//  Cprimerplusplus
+//
+//  Created by junwuli on 2018/8/21.
+//  Copyright © 2018 GDUT. All rights reserved.
+//
+
+//#include <stdio.h>
+//#include <vector>
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//    vector<int>v1{1,2,3,4};
+//    vector<int>v2{2,3,5};
+//    vector<int>v3{3,5,7};
+//    
+//    if(v2>v3)
+//        cout<<"v2>v3"<<endl;
+//    else if(v2<v3)
+//        cout<<"v2<v3"<<endl;
+//    else
+//        cout<<"v2=v3"<<endl;
+//   
+//    return 0;
+//}
