57ae2f853d390acdb45174b9e09f6cf7e5aba0e6
junwu <junwuli@foxmail.com>
2018-08-28 14:28:02 +0800

    ex952

---
+++ Cprimerplusplus/ex952.cpp
@@ -0,0 +1,40 @@
+//
+//  ex952.cpp
+//  Cprimerplusplus
+//
+//  Created by junwuli on 2018/8/28.
+//  Copyright © 2018 GDUT. All rights reserved.
+//
+
+#include <stack>
+#include <string>
+#include <iostream>
+
+using std::string; using std::cout; using std::endl; using std::stack;
+
+int main()
+{
+    string expression{ "This is (pezy)." };
+    bool bSeen = false;
+    stack<char> stk;
+    for (const auto &s : expression)
+    {
+        if (s == '(') { bSeen = true; continue; }
+        else if (s == ')') bSeen = false;
+        
+        if (bSeen) stk.push(s);
+    }
+    
+    string repstr;
+    while (!stk.empty())
+    {
+        repstr += stk.top();
+        stk.pop();
+    }
+    
+    expression.replace(expression.find("(")+1, repstr.size(), repstr);
+    
+    cout << expression << endl;
+    
+    return 0;
+}
