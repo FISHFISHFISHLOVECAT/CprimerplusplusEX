6e2d10124fff7b03d276092fa5886118ae826572
junwu <junwuli@foxmail.com>
2018-09-02 19:34:10 +0800

    add public hearder

---
+++ publicHeader.h
@@ -0,0 +1,16 @@
+//
+//  publicHeader.h
+//  Cprimerplusplus
+//
+//  Created by junwuli on 2018/9/2.
+//  Copyright © 2018 GDUT. All rights reserved.
+//
+
+#ifndef publicHeader_h
+#define publicHeader_h
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+#endif /* publicHeader_h */
